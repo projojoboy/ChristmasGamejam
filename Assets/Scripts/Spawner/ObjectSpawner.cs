846c359b5cfa9df66969bd77244c2581b5914974
Projojoboy <22048@ma-web.nl>
2018-12-21 10:43:40 +0100

    Added Script to spawn the falling objects

---
+++ Assets/Scripts/Spawner/ObjectSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] spawnableObjects;
+
+    [SerializeField] private int minRange = -9, maxRange = 9;
+
+    public void SpawnObject()
+    {
+        //spawn 9
+        int randInt = Random.Range(minRange, maxRange);
+        int randGO = Random.Range(0, spawnableObjects.Length);
+
+        Instantiate(spawnableObjects[randGO], new Vector2(transform.position.x + randInt, transform.position.y), Quaternion.identity);
+    }
+}
