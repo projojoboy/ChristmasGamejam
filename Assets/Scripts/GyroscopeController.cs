267226486bfaad0f21f7db52edf57ec0cdef0bee
Projojoboy <22048@ma-web.nl>
2018-12-21 09:56:13 +0100

    Added Gyroscope Movement

---
+++ Assets/Scripts/GyroscopeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class GyroscopeController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector2(Input.acceleration.x * speed, 0));
+    }
+}
